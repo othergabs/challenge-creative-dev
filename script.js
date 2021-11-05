9147543e0532452259986503229b53ecfe4919e7
Gabriela Fonseca <gabriela.fonseca@idwall.co>
2021-11-05 13:36:30 -0300

    chore: testing new script

--- script.js
+++ script.js
@@ -1,21 +1,35 @@
-const cardTemplate = document.querySelector('[data-id="card-template"]').innerHTML;
+const cardTemplate = document.querySelector(
+  '[data-id="card-template"]'
+).innerHTML;
 const $cardsWrapper = document.querySelector('[data-id="cards-wrapper"]');
 
-const defaultAnimal = 'dogs';
+const defaultAnimal = "cats";
 
 const populateCards = async (selectedAnimal) => {
-  const animals = await fetch('mock.json')
-    .then(res => res.json())
-    
-  const currentAnimal = animals[selectedAnimal]
+  $cardsWrapper.innerHTML = "";
+  const animals = await fetch("mock.json")
+    .then((res) => res.json())
+    .then(
+      (value) =>
+        new Promise((resolve) => {
+          setTimeout(() => {
+            resolve(value);
+          }, 1500);
+        })
+    );
 
-  const cards = currentAnimal.map((animal) => 
-    cardTemplate.replace('{{image}}', animal.image)
-      .replace('{{title}}', animal.title)
-      .replace('{{content}}', animal.content)
-  ).join('');
+  const currentAnimal = animals[selectedAnimal];
+
+  const cards = currentAnimal
+    .map((animal) =>
+      cardTemplate
+        .replace("{{image}}", animal.image)
+        .replace("{{title}}", animal.title)
+        .replace("{{content}}", animal.content)
+    )
+    .join("");
 
   $cardsWrapper.innerHTML = cards;
-}
+};
 
-populateCards(defaultAnimal)
+populateCards(defaultAnimal);
